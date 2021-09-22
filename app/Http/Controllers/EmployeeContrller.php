d97c769dcfdfac916b080d6c8842821bcb61733a
Munira Akter <munira660602@gmail.com>
2021-09-22 16:22:08 +0600

    Make Employeer Model Migration

--- app/Http/Controllers/EmployeeContrller.php
+++ app/Http/Controllers/EmployeeContrller.php
@@ -7,5 +7,9 @@ use Illuminate\Http\Request;
 class EmployeeContrller extends Controller
 {
     //store Employee
-    
+
+    public function store (Request $request){
+        return $request -> all;
+    }
+
 }
--- database/migrations/2021_09_22_101558_create_employees_table.php
+++ database/migrations/2021_09_22_101558_create_employees_table.php
@@ -15,6 +15,13 @@ class CreateEmployeesTable extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('photo');
+            $table->string('salary');
+            $table->string('role');
+            $table->string('password');
             $table->timestamps();
         });
     }
