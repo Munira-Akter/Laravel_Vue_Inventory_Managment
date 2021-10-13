b4f3366211410c88c0b7b66059f58d05d4f9913d
Munira Akter <munira660602@gmail.com>
2021-10-13 12:43:27 +0600

    Make product model migration and controller

---
+++ database/migrations/2021_10_13_063950_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('code')->nullable();
+            $table->string('root')->nullable();
+            $table->string('photo')->nullable();
+            $table->string('buying_date')->nullable();
+            $table->string('buying_price');
+            $table->string('selling_price');
+            $table->integer('category_id')->nullable();
+            $table->integer('supplier_id')->nullable();
+            $table->string('qty');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
